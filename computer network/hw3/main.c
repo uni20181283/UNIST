2c1358063fbab4d050b4572c08a1b80168b8f2fc
uni20181283 <76513688+uni20181283@users.noreply.github.com>
2021-12-26 20:52:33 +0900

    Add files via upload

---
+++ computer network/hw3/main.c
@@ -0,0 +1,322 @@
+// hw3 20181283 HWANG GYOHUN
+// Implementing an HTTP proxy
+/*  Accept an absolute HTTP requests from clients
+        GET http://www.cs.princeton.edu/index.html HTTP/1.0
+    Forward requests to origin servers (with relative URLs)
+        GET /index.html HTTP/1.0
+        Host: www.cs.princeton.edu
+        Connection: close (need to add this header or change to this if needed) (other headers can be just forwarded)
+    Return response data to a client
+    
+    < cases for invalid >
+    An invalid request from the client should be answered with an error code, i.e. "Bad Request" (400)
+        or "Not Implemented" (501) for valid HTTP methods other than GET
+
+    Should run using a command like “proxy <port number>”
+
+    1. Accept
+    2. Forward -> proxy server로
+    3. return response data
+
+*/
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // for strlen
+
+#include <sys/select.h> // fd_set, timeval
+
+#define origin_port 80
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+int is_valid(char abs_req[]);
+
+
+int main(int argc, char *argv[]){
+    int port_number = atoi(argv[1]);
+    // declare socket
+    int proxy_sock;
+    int sock1, sock2;
+    // declare ip/port
+    struct sockaddr_in sock1_client, sock2_client;
+    struct sockaddr_in proxy;
+
+    memset(&proxy,0,sizeof(proxy));
+    proxy.sin_family = AF_INET;
+    proxy.sin_port = htons(port_number);
+    proxy.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // 1. socket()
+    proxy_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(proxy_sock < 0){
+        printf("CAN'T SOCKET\n");
+        abort();
+    }
+    // Reusing the same port ???
+    // 2. bind()
+    if(bind(proxy_sock, (struct sockaddr*)&proxy, sizeof(proxy)) < 0){
+        printf("CAN'T BIND\n");
+        abort();
+    }
+    
+    // 3. listen()
+    if(listen(proxy_sock, 32) < 0){
+        printf("CAN'T LISTEN");
+        abort();
+    }
+    // 4. accept()
+    socklen_t client_size_1 = sizeof(sock1_client);
+    sock1 = accept(proxy_sock, (struct sockaddr*)&sock1_client, &client_size_1);
+    if(sock1 < 0){
+        printf("CAN'T ACCEPT\n");
+        abort();
+    }
+    // 4. accept()
+    socklen_t client_size_2 = sizeof(sock2_client);
+    sock2 = accept(proxy_sock, (struct sockaddr*)&sock2_client, &client_size_2);
+    if(sock2 < 0){
+        printf("CAN'T ACCEPT\n");
+        abort();
+    }
+
+    fd_set read_set;
+    struct timeval timeout;
+
+    FD_ZERO(&read_set);
+    FD_SET(sock1, &read_set);
+    FD_SET(sock2, &read_set);
+
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 5000;
+
+    if(select(MAX(sock1, sock2) + 1, &read_set, NULL, NULL, &timeout) < 0){
+        printf("SELCET ERROR\n");
+        abort();
+    }
+    if(FD_ISSET(sock1, &read_set)){
+        // sock1 has data
+        char client_buffer[4096];
+        int expected_data_len = sizeof(client_buffer);
+        int client_receive = recv(sock1, client_buffer, expected_data_len, 0);
+        if(client_receive < 0){
+            printf("RECEIVE ERROR\n");
+            return 1;
+        }
+        unsigned int i = 0;
+        while(client_buffer[i] != '\n'){
+            i++;
+        }
+        client_buffer[i] = '\0';
+        int valid = is_valid(client_buffer);
+        if(valid == 0){
+            char *ptr = strtok(client_buffer, " ");
+            char *origin_server;
+            char *relative_url;
+            if(ptr != NULL) origin_server = (ptr = strtok(NULL, " "));
+            else return 1;
+            if(ptr != NULL) relative_url = (ptr = strtok(NULL, "\n"));
+            else return 1;
+            origin_server = strchr(origin_server, 'w');
+            
+            // GET http://www.gnu.org HTTP/1.0
+            // GET https://www.gnu.org HTTP/1.0
+            // GET http://www.gnu.org/ HTTP/1.0
+            //  -> www.gnu.org
+            //  -> HTTP/1.0
+            char send_msg[4096] = "";
+            strcat(send_msg, "GET ");
+            strcat(send_msg, relative_url);
+            strcat(send_msg, "\nHost: ");
+            strcat(send_msg, origin_server);
+            strcat(send_msg, "\nConnection: close\n\n");
+            FILE *file;
+            char *filename = origin_port + ".html";
+            file = fopen(filename, "r");
+            if(file){
+                int send_sock = socket(AF_INET, SOCK_STREAM, 0);
+                if(send_sock < 0){
+                    printf("CAN'T SEND_SOCK\n");
+                    return 1;
+                }
+                // 6. write
+                // printf("%s\n", origin_buffer);
+                write(sock1, file, sizeof(file));
+                fclose(file);
+                close(send_sock);
+            }
+            else{
+                fclose(file);
+                file = fopen(filename, "w");
+                struct sockaddr_in origin;
+                struct hostent *server;
+                if((server = gethostbyname(origin_server)) == NULL){
+                    printf("SERVER ERROR\n");
+                    return 1;
+                }
+                memset(&origin,0,sizeof(origin));
+                origin.sin_port = htons(origin_port);
+                origin.sin_family = AF_INET;
+                origin.sin_addr.s_addr = *((unsigned long *)server->h_addr);
+                int send_sock = socket(AF_INET, SOCK_STREAM, 0);
+                if(send_sock < 0){
+                    printf("CAN'T SEND_SOCK\n");
+                    return 1;
+                }
+                if (connect(send_sock, (struct sockaddr*)&origin, sizeof(origin)) < 0) {
+                    printf("CAN'T CONNECT\n");
+                    return 1;
+                }
+                int send_msg_size = sizeof(send_msg);
+                int send_byte = send(send_sock, send_msg, send_msg_size, 0);
+                if(send_byte < 0){
+                    printf("SEND ERRER\n");
+                    return 1;
+                }
+                char origin_buffer[4096] = "";
+                expected_data_len = sizeof(origin_buffer);
+                int origin_bytes = recv(send_sock, origin_buffer, expected_data_len, 0);
+                if(origin_bytes < 0){
+                    printf("RECIEVE ERROR\n");
+                    return 1;
+                }
+                fputs(origin_buffer, file);
+                fclose(file);
+                // 6. write
+                // printf("%s\n", origin_buffer);
+                write(sock1, origin_buffer, expected_data_len);
+                close(send_sock);   
+            }
+        }
+        else if(valid == 1)
+        write(sock1, "Bad Request(400)\n", sizeof("Bad Request(400)\n"));
+        else
+        write(sock1, "Not Implemented(501)\n", sizeof("Not Implemented(501)\n"));
+    }
+    if(FD_ISSET(sock2, &read_set)){
+        // sock2 has data
+        char client_buffer[4096];
+        int expected_data_len = sizeof(client_buffer);
+        int client_receive = recv(sock2, client_buffer, expected_data_len, 0);
+        if(client_receive < 0){
+            printf("RECEIVE ERROR\n");
+            return 1;
+        }
+        unsigned int i = 0;
+        while(client_buffer[i] != '\n'){
+            i++;
+        }
+        client_buffer[i] = '\0';
+        int valid = is_valid(client_buffer);
+        if(valid == 0){
+            char *ptr = strtok(client_buffer, " ");
+            char *origin_server;
+            char *relative_url;
+            if(ptr != NULL) origin_server = (ptr = strtok(NULL, " "));
+            else return 1;
+            if(ptr != NULL) relative_url = (ptr = strtok(NULL, "\n"));
+            else return 1;
+            origin_server = strchr(origin_server, 'w');
+            
+            // GET http://www.gnu.org HTTP/1.0
+            // GET https://www.gnu.org HTTP/1.0
+            // GET http://www.gnu.org/ HTTP/1.0
+            //  -> www.gnu.org
+            //  -> HTTP/1.0
+            char send_msg[4096] = "";
+            strcat(send_msg, "GET ");
+            strcat(send_msg, relative_url);
+            strcat(send_msg, "\nHost: ");
+            strcat(send_msg, origin_server);
+            strcat(send_msg, "\nConnection: close\n\n");
+
+            struct sockaddr_in origin;
+            struct hostent *server;
+            if((server = gethostbyname(origin_server)) == NULL){
+                printf("SERVER ERROR\n");
+                return 1;
+            }
+            memset(&origin,0,sizeof(origin));
+            origin.sin_port = htons(origin_port);
+            origin.sin_family = AF_INET;
+            origin.sin_addr.s_addr = *((unsigned long *)server->h_addr);
+            int send_sock = socket(AF_INET, SOCK_STREAM, 0);
+            if(send_sock < 0){
+                printf("CAN'T SEND_SOCK\n");
+                return 1;
+            }
+            if (connect(send_sock, (struct sockaddr*)&origin, sizeof(origin)) < 0) {
+                printf("CAN'T CONNECT\n");
+                return 1;
+            }
+            int send_msg_size = sizeof(send_msg);
+            int send_byte = send(send_sock, send_msg, send_msg_size, 0);
+            if(send_byte < 0){
+                printf("SEND ERRER\n");
+                return 1;
+            }
+            char origin_buffer[4096] = "";
+            expected_data_len = sizeof(origin_buffer);
+            int origin_bytes = recv(send_sock, origin_buffer, expected_data_len, 0);
+            if(origin_bytes < 0){
+                printf("RECIEVE ERROR\n");
+                return 1;
+            }
+            // 6. write
+            // printf("%s\n", origin_buffer);
+            write(sock2, origin_buffer, expected_data_len);
+            close(send_sock);
+        }
+        else if(valid == 1)
+        write(sock2, "Bad Request(400)\n", sizeof("Bad Request(400)\n"));
+        else
+        write(sock2, "Not Implemented(501)\n", sizeof("Not Implemented(501)\n"));
+    }
+    // 8. close
+    close(sock1);
+    close(sock2);
+    close(proxy_sock);
+
+    return 0;
+}
+
+
+
+int is_valid(char abs_req[]){
+    char right_req[11];
+    right_req[0] = 'G';
+    right_req[1] = 'E';
+    right_req[2] = 'T';
+    right_req[3] = ' ';
+    right_req[4] = 'h';
+    right_req[5] = 't';
+    right_req[6] = 't';
+    right_req[7] = 'p';
+    right_req[8] = ':';
+    right_req[9] = '/';
+    right_req[10] = '/';
+    // EX)  GET http://www.gnu.org/ HTTP/1.0
+    //      GET http://www.gnu.org HTTP/1.0
+    if(strlen(abs_req) < sizeof(right_req)){
+        return 1;
+    }
+    if(!(abs_req[0] == 'G' && abs_req[1] == 'E' && abs_req[2] == 'T')){
+        return 2;
+    }
+    if(abs_req[9] == 's'){
+        return 2;
+    }
+    for(int i = 0; i < sizeof(right_req); i ++){
+        if(abs_req[i] != right_req[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Save에 대한 부분 : 
+// I/O multiplexing에 대한 부분 - ppt에 있음(이해 해야할 듯)
